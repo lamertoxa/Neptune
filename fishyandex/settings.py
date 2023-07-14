3f286a4c121e4659d16f04f481e6ae126231d0de
Anton Sliusar <63955212+lamertoxa@users.noreply.github.com>
2023-07-14 15:15:06 +0300

    Update settings.py

--- fishyandex/settings.py
+++ fishyandex/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qwm&xp)+m1xqp84ub0s$s9(0hc^q8^c2xwj46ebwpg@ea!^9#e'
+SECRET_KEY = 'KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
